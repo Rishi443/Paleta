b0478928f2271dec4d19612563b6e09f0464caaf
ericc0429 <66334823+ericc0429@users.noreply.github.com>
2021-04-11 14:24:30 -0500

    Added files for homepage, doesn't have updated xml file

---
+++ app/src/main/java/com/example/paleta/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.paleta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bCP, p1, p2, p3, p4, p5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Palette Creation
+        bCP = (Button) findViewById(R.id.bCP);
+        bCP.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                openCP();
+            }
+        });
+
+        // Palette Editor Buttons
+        p1 = (Button) findViewById(R.id.p1);
+        p1.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                openPE();
+            }
+        });
+        p2 = (Button) findViewById(R.id.p2);
+        p2.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                openPE();
+            }
+        });
+        p3 = (Button) findViewById(R.id.p3);
+        p3.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                openPE();
+            }
+        });
+        p4 = (Button) findViewById(R.id.p4);
+        p4.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                openPE();
+            }
+        });
+        p5 = (Button) findViewById(R.id.p5);
+        p5.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                openPE();
+            }
+        });
+
+    }
+    // Links to Palette Creator Tool
+    public void openCP() {
+        Intent intent = new Intent( this, Activity2.class );
+        startActivity( intent );
+    }
+    // Links to Palette Editor Tool -- Consider passing a Palette class in with the saved color data.
+    public void openPE() {
+        Intent intent = new Intent( this, Activity3.class );
+        startActivity( intent );
+    }
+}
