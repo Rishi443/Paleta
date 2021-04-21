e1813fa630172afeca568550d0c8c1b46cca3f49
ericc0429 <66334823+ericc0429@users.noreply.github.com>
2021-04-21 13:12:13 -0500

    Updated homepage with latest XML file

--- app/src/main/java/com/example/paleta/MainActivity.java
+++ app/src/main/java/com/example/paleta/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.paleta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     private Button bCP, p1, p2, p3, p4, p5;
