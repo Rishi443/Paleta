dbc4a61f3edb7afd47efe69c38bc3111b16b441a
ericc0429 <66334823+ericc0429@users.noreply.github.com>
2021-04-30 10:22:10 -0500

    finished fixing export

--- app/src/main/java/com/example/paletapaletteeditor/MainActivity.java
+++ app/src/main/java/com/example/paletapaletteeditor/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.paletapaletteeditor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        openExport();
+                        openEmailExport();
                     }
                 }
         );
@@ -40,5 +42,20 @@ public class MainActivity extends AppCompatActivity {
 
         Intent shareIntent = Intent.createChooser( sendIntent, null );
         startActivity( shareIntent );
+    }
+    public void openEmailExport() {
+
+        String output = "Color 1: #22e8f2 \nColor 2: #e8b254 \nColor 3: #eb215a \nColor 4: #036b6b \nColor 5: #66bda4 \nColor 6:  \nColor 7:  \nColor 8:  \n";
+
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"recipient@example.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "Paleta");
+        i.putExtra(Intent.EXTRA_TEXT   , output );
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
         }
+    }
 }
